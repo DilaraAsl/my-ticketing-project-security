ab7e95b3bf97f85c9e557db9c48d3efe79f80048
DilaraAsl <dcavus@gmail.com>
2022-11-02 20:23:01 -0700

    gitignore committed

---
+++ src/main/java/com/cydeo/MyTicketingProjectDataNewApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTicketingProjectDataNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTicketingProjectDataNewApplication.class, args);
+    }
+
+}
